76c5ee66f59152cee618601e22ac8932220dcc8f
IkigaiRamen <55280193+IkigaiRamen@users.noreply.github.com>
2023-10-31 22:17:22 +0100

    cors

--- src/main/java/com/esprit/websemanticstroc/Controllers/SparqlController.java
+++ src/main/java/com/esprit/websemanticstroc/Controllers/SparqlController.java
@@ -6,12 +6,14 @@ import org.apache.jena.query.ResultSetFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayOutputStream;
 
 @RestController
+@CrossOrigin("*")
 public class SparqlController {
 
     @Autowired
